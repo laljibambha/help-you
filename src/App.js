2552de7845600876c8a06ab5a136399248957dd1
laljibambha <151505803+laljibambha@users.noreply.github.com>
2024-02-06 16:07:26 +0530

    message

--- src/App.js
+++ src/App.js
@@ -1,40 +1,36 @@
-import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Login from "./Login";
-import Home from "./components/Home";
-import Dashboard from "./components/Dashboard";
-import Service from "./components/Service";
-import User from "./components/User";
-import Category from "./components/Category";
-import "./App.css";
-import SubCategory from "./components/Sub_Category";
-import Product from "./components/Product";
-import Order from "./components/Order";
-import Help from "./components/Help"; 
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './Login';
+import Home from './components/Home';
+import Dashboard from './components/Dashboard';
+import Service from './components/Service';
+import User from './components/User';
+import Category from './components/Category';
+import './App.css';
+import SubCategory from './components/Sub_Category';
+import Product from './components/Product';
+import Order from './components/Order';
+import Help from './components/Help'; 
 
 function App() {
-  const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState('light');
 
   const toggleTheme = () => {
-    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
   };
 
   useEffect(() => {
-    // You can save the theme preference in local storage or use a more advanced state management solution
-    // For simplicity, this example uses local storage
-    localStorage.setItem("theme", theme);
+    localStorage.setItem('theme', theme);
   }, [theme]);
 
   useEffect(() => {
-    // Retrieve the theme preference from local storage when the component mounts
-    const savedTheme = localStorage.getItem("theme");
+    const savedTheme = localStorage.getItem('theme');
     if (savedTheme) {
       setTheme(savedTheme);
     }
   }, []);
 
   return (
-    
     <Router>
       <div className={`app ${theme}`}>
         <Routes>
@@ -59,4 +55,4 @@ function App() {
   );
 }
 
-export default App;
+export default App; // Export App as default
